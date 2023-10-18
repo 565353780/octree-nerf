1bf78e9b5b3dda07f31872d37bfe7a31f678f935
chLi <565353780@qq.com>
2023-10-18 23:46:23 +0800

    add setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,14 @@
+pip install rich tqdm ninja torch numpy scipy \
+	lpips pandas trimesh PyMCubes torch-ema \
+	dearpygui packaging matplotlib tensorboardX \
+	opencv-python imageio imageio-ffmpeg pymeshlab \
+	xatlas scikit-learn torchmetrics
+
+# torch-scatter
+pip install torch_efficient_distloss
+pip install git+https://github.com/NVlabs/nvdiffrast/
+
+pip install ./raymarching
+pip install ./gridencoder
+pip install ./freqencoder
+pip install ./shencoder
